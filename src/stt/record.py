2709cca420442b6680c43cb8251da5df97352142
BuildTools <fouts.57@osu.edu>
2021-05-27 17:08:39 -0400

    Actually integrated speech to text this time

---
+++ src/stt/record.py
@@ -0,0 +1,64 @@
+import pyaudio
+import wave
+
+
+class Recorder:
+    def __init__(self, max_sec=5):
+
+        # Max recording size if user doesn't stop
+        self.max_seconds = max_sec
+
+        # Changing these is not recommended
+        self.chunk_size = 1024
+        self.audio_format = pyaudio.paInt16
+        self.num_channels = 1
+        self.rate_hz = 44100
+
+        self.p = pyaudio.PyAudio()
+
+        self.stream = self.p.open(format=self.audio_format,
+                                  channels=self.num_channels,
+                                  rate=self.rate_hz,
+                                  input=True,
+                                  frames_per_buffer=self.chunk_size)
+
+    # Pass in a function that returns true when audio recording should stop
+    def record(self, interruptFunction=None):
+        print("---RECORDING---")
+
+        self.frames = []
+
+        for i in range(0, int(self.rate_hz / self.chunk_size * self.max_seconds)):
+            if interruptFunction is not None and interruptFunction():
+                break
+            data = self.stream.read(self.chunk_size)
+            self.frames.append(data)
+
+        print("---DONE RECORDING---")
+
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+        # returns (frames, rate, channels)
+        return (b''.join(self.frames), self.rate_hz, self.num_channels)
+
+    # Outputs as file for debugging purposes
+    def send_to_file(self, output_file="output.wav"):
+        wf = wave.open(output_file, 'wb')
+        wf.setnchannels(self.num_channels)
+        wf.setsampwidth(self.p.get_sample_size(self.audio_format))
+        wf.setframerate(self.rate_hz)
+        wf.writeframes(b''.join(self.frames))
+        wf.close()
+
+
+''' 
+
+EXAMPLE USAGE
+
+r = Recorder()
+frames = r.record(quitOnKeypress) # where quitOnKeypress is externally defined
+r.send_to_file()
+
+'''
---
+++ src/stt/watson_stt.py
@@ -0,0 +1,49 @@
+import json
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import ApiException
+from os.path import join, dirname
+from stt.record import Recorder
+
+
+class SpeechToText:
+    # these are the credentials associated with 'massouh.3@osu.edu'
+    # you may initialize the speech-to-text service with your own credentials
+    default_key = 'R9hW3VQy4vgbFAHYoq9WWnzKoI5QioBVH9UAsWovlwVk'
+    default_loc = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/1aafab97-84e5-4963-8fdc-8d078b522a15'
+
+    def __init__(self, ssl_disable=False, key=default_key, loc=default_loc):
+
+        authenticator = IAMAuthenticator(key)
+
+        self.speech_to_text = SpeechToTextV1(
+            authenticator=authenticator
+        )
+
+        self.speech_to_text.set_service_url(loc)
+
+        self.speech_to_text.set_disable_ssl_verification(ssl_disable)
+
+    def recognize_speech(self, frames, rate, channels):
+
+        try:
+            speech_recognition_results = self.speech_to_text.recognize(
+                audio=frames,
+                content_type=f"audio/l16;rate={rate};channels={channels}",
+            ).get_result()
+            return speech_recognition_results.get("results")[0]['alternatives']
+        except ApiException as ex:
+            print("Function failed with status code " +
+                  str(ex.code) + ": " + ex.message)
+
+
+'''
+# EXAMPLE USAGE
+
+s = SpeechToText()
+r = Recorder()
+frames, rate, channels = r.record()
+r.send_to_file()
+print(s.recognize_speech(frames, rate, channels))
+# [{'transcript': "Mary had a little lamb who's fleece was white as snow ", 'confidence': 0.99}]
+'''
