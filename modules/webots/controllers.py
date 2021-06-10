82bc183d6d302aef31f84ee8cd11554b2a1e2d55
BuildTools <fouts.57@osu.edu>
2021-06-10 17:07:10 -0400

    Implemented textual robot controller

--- modules/webots/controllers.py
+++ modules/webots/controllers.py
@@ -3,59 +3,115 @@ Created by: Craig Fouts
 Created on: 6/10/2021
 '''
 
-
-# class ControllerAdapter:
-
-#     def __init__(self, controller):
-#         self.controller = controller()
-#         methods = {method: getattr(self.controller, method) for method in
-#                    dir(self.robot) if not method.startswith('_')}
-#         self.__dict__.update(methods)
-
-#     def __getattr__(self, name):
-#         return getattr(self.robot, name)
+from abc import ABC, abstractmethod
+from webots.controller import Robot
 
 
-class ControllerAdapter:
+class RobotController:
 
-    def __init__(self, controller, **attributes):
-        self.controller = controller()
-        self.__dict__.update(attributes)
+    def __init__(self, robot):
+        self.robot = robot()
+        methods = {method: getattr(self.robot, method) for method in
+                   dir(self.robot) if not method.startswith('_') and
+                   method.endswith('_')}
+        self.__dict__.update(methods)
 
     def __getattr__(self, name):
-        return getattr(self.controller, name)
+        return getattr(self.robot, name)
 
-    def move_forward(self, distance, speed):
-        self.controller.move_forward(distance, speed)
 
-    def move_backward(self, distance, speed):
-        self.controller.move_backward(distance, speed)
+class AbstractController(ABC):
 
-    def strafe_right(self, distance, speed):
-        self.controller.strafe_right(distance, speed)
+    def __init__(self):
+        super().__init__()
 
-    def strafe_left(self, distance, speed):
-        self.controller.strafe_left(distance, speed)
+    @abstractmethod
+    def drive_forward_(self, distance, speed): pass
 
-    def turn_right(self, degrees, speed):
-        self.controller.turn_right(degrees, speed)
+    @abstractmethod
+    def drive_backward_(self, distance, speed): pass
 
-    def turn_left(self, degrees, speed):
-        self.controller.turn_left(degrees, speed)
+    @abstractmethod
+    def strafe_right_(self, distance, speed): pass
 
-    def raise_arm(self, distance, speed):
-        self.controller.raise_arm(distance, speed)
+    @abstractmethod
+    def strafe_left_(self, distance, speed): pass
 
-    def lower_arm(self, distance, speed):
-        self.controller.lower_arm(distance, speed)
+    @abstractmethod
+    def turn_right_(self, degrees, speed): pass
 
-    def open_fingers(self, distance, speed):
-        self.controller.open_fingers(distance, speed)
+    @abstractmethod
+    def turn_left_(self, degrees, speed): pass
 
-    def close_fingers(self, distance, speed):
-        self.controller.close_fingers(distance, speed)
+    @abstractmethod
+    def raise_arm_(self, distance, speed): pass
+
+    @abstractmethod
+    def lower_arm_(self, distance, speed): pass
+
+    @abstractmethod
+    def open_fingers_(self): pass
+
+    @abstractmethod
+    def close_fingers_(self): pass
+
+
+class TextRobot(AbstractController):
+
+    def __init__(self):
+        super().__init__()
+
+    def drive_forward_(self, distance, speed):
+        output = f'Driving forward distance {distance} at speed {speed}'
+        print(output)
+        return output
+
+    def drive_backward_(self, distance, speed):
+        output = f'Driving backward distance {distance} at speed {speed}'
+        print(output)
+        return output
+
+    def strafe_right_(self, distance, speed):
+        output = f'Strafing right distance {distance} at speed {speed}'
+        print(output)
+        return output
+
+    def strafe_left_(self, distance, speed):
+        output = f'Strafing left distance {distance} at speed {speed}'
+        print(output)
+        return output
+
+    def turn_right_(self, degrees, speed):
+        output = f'Turning right {degrees} degrees at speed {speed}'
+        print(output)
+        return output
+
+    def turn_left_(self, degrees, speed):
+        output = f'Turning left {degrees} degrees at speed {speed}'
+        print(output)
+        return output
+
+    def raise_arm_(self, distance, speed):
+        output = f'Raising arm distance {distance} at speed {speed}'
+        print(output)
+        return output
+
+    def lower_arm_(self, distance, speed):
+        output = f'Lowering arm distance {distance} at speed {speed}'
+        print(output)
+        return output
+
+    def open_fingers_(self):
+        output = 'Opening fingers'
+        print(output)
+        return output
+
+    def close_fingers_(self):
+        output = 'Closing fingers'
+        print(output)
+        return output
 
 
 if __name__ == '__main__':
-    controller = ControllerAdapter(TestRobot)
-    controller.say('Hello, World')
+    controller = RobotController(TextRobot)
+    controller.drive_forward_(100, 10)
