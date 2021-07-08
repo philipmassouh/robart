f9969c7256eddd6f972d0b09f431a6fc4a1c3625
philipmassouh <massouh.3@osu.edu>
2021-07-08 13:46:39 -0400

    ui flex update

--- webapp/scripts/chat.js
+++ webapp/scripts/chat.js
@@ -2,14 +2,21 @@ function watsonChat(message, item, numButtons) {
     let chatbox = document.getElementByClass("chatbox");
     let bubble = document.createElement("div");
     bubble.classList.add("robart")
-    bubble.textContent(string[0])
-    if (numButtons > 1) {
+    bubble.textContent(message)
+
+    if (item.length > 1) {
         let options = string.split(",");
         let buttons = document.createElement("div");
+
         for (i = 0; i < numBubbles; i++) {
             let button = document.createElement("input");
+            button.id.add("TextToSend")
             button.classList.add("btn")
+            //TODO one of these is redundant
+            button.textContent(item[i])
+            button.value = "Get" + item[i]
         }
+
         chatbox.append(buttons)
     }
     chatbox.append(bubble)
